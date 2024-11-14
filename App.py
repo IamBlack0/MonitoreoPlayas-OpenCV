5bc83df01b1191c9e40486ffa138d347af629753
black <blacknegocio3@gmail.com>
2024-11-13 20:21:05 -0500

    Actualizar configuración de la aplicación para usar wsgi y modificar el comando en Procfile

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 # app.py
+import os
 from init import app
 from routes import *
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=False)
